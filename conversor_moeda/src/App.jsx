20d939eb703e64e540c348165bb30acadb415edb
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-06-11 16:09:47 -0300

    feat: setup inicial -Conversor de Moedas- -React JS-

---
+++ conversor_moeda/src/App.jsx
@@ -0,0 +1,14 @@
+import CurrencyConverter from './components/CurrencyConverter'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <CurrencyConverter />
+    </>
+  )
+}
+
+export default App
---
+++ conversor_moeda/src/components/CurrencyConverter.jsx
@@ -0,0 +1,9 @@
+import "./CurrencyConverter.jsx"
+
+const CurrencyConverter = () => {
+    return (
+        <div><h1>CurrencyCOnverter</h1></div>
+    )
+}
+
+export default CurrencyConverter;
